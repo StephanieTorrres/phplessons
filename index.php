0972745fc5395a693f83e05a1c6ec86b3559474f
stephanie torres <110932086+StephanieTorrres@users.noreply.github.com>
2024-03-21 08:04:56 -0500

    g

--- index.php
+++ index.php
@@ -12,9 +12,10 @@
     <button>SUBMIT</button>
 </form>
     <?php
-    $name = $_GET['person'];
-    echo $name. "is a handsome fellow ";
-   
+    //This gets something from the database
+    function() {
+        flowers}
+     
     ?>
 
 </body>
