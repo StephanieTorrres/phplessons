f59bd0e67e3815eca9662e61148c7268e1278f8e
stephanie torres <110932086+StephanieTorrres@users.noreply.github.com>
2024-03-22 08:30:15 -0500

    h

---
+++ datatypes.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //String
+    $name = 'Coding is exciting';
+
+    //Interger
+    $name = 20;
+
+
+    //Float
+    $name = 20.5784;
+
+    //Boolean
+    
+    //Array
+    $names = array("Charlotte","Mary","Adaline");
+    echo $names['2']
+
+    
+    ?>
+</body>
+</html>
