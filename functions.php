f5fb43eea9a8bbddbbefa8d4afc37c6a66861457
stephanie torres <110932086+StephanieTorrres@users.noreply.github.com>
2024-04-03 07:30:08 -0500

    f

--- functions.php
+++ functions.php
@@ -11,5 +11,27 @@
 echo str_replace( "Daniel", "Charlottle","Hi Daniel",);
 
 ?>
+<?php
+
+
+
+function newCalc($x) {
+    $newnr = $x * 0.75;
+    echo "Here is 75% of what you wrote: ".$newnr;
+
+}
+
+
+$x = 100;
+newCalc($x);
+
+echo "<br>";
+
+$a = 10;
+newCalc($a);
+
+
+
+?>
 </body>
 </html>
---
+++ loops.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //Loop
+
+    //White loop
+    //Do while loop
+    //For loop
+    //Foreach loop
+    $x = 1;
+    while ($x < 5) {
+        echo "hi there<br>";
+        $x++;
+    }?>
+    <?php
+    //White loop
+    //Do while loop
+    //For loop
+    //Foreach loop
+    $x = 1;
+    do {
+        echo "hi there<br>";
+        $x++;
+ }
+    while ($x <= 5); 
+        ?>
+
+    <?php
+    //White loop
+    //Do while loop
+    //For loop
+    //Foreach loop
+    for ($x = 0; $x <= 10; $x++) {
+        echo "hi<br>";
+    }
+        ?>
+         <?php
+         //Loop
+
+    //White loop
+    //Do while loop
+    //For loop
+    //Foreach loop
+    $array = array("Daniel", "Jane", "Jacob","John","Mariane");
+    foreach($array as $loopdata) {
+        echo "My name is ".$loopdata."<br>";
+    }         
+          ?>
+</body>
+</html>
