24e87019f25d80d3cb68ae6f00b4d9383ef454c2
stephanie torres <110932086+StephanieTorrres@users.noreply.github.com>
2024-04-04 08:23:51 -0500

    gf

--- calculator.php
+++ calculator.php
@@ -1,26 +1,22 @@
-<!DOCTYPE html>
+
+
+
+
+
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form>
-        <input type="text" name="num1" placeholder= "Number1">
-        <input type="text" name="num2" placeholder= "Number2">
-<select name="operator">
-    <option>None</option>
-    <option>Add</option>
-    <option>Subtarct</option>
-    <option>Multiply</option>
-    <option>Divide</option>
-    <select>
-        <br>
-        <button type="submit" name="submit" value="submit">Calculate</button>
-</form>
-<p>The answer is:</p>
-<?php
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+  <?php
+    include 'includes/header.php'
+    
+    ?>
+  <?php
 if (isset($_GET['submit'])) {
     $result1 = $_GET['num1'];
     $result2 = $_GET['num2'];
@@ -45,8 +41,6 @@ if (isset($_GET['submit'])) {
 }
 
 ?>
-    
-    
-  
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
 </html>
---
+++ includes/header.php
@@ -0,0 +1,69 @@
+
+
+
+
+
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Title of the document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+
+  <body>
+
+    <header>
+    <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="calculator.php">Home</a>
+    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="calendar.php">Calendar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="conditionals.php">Condtionals</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="datatypes.php">Datatypes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="functions.php">Functions</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Index</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="loops.php">Loops</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="operators.php">Operators</a>
+        </li>
+</ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    </header>
+    
+    <section>
+        <div class="someMainContent">
+            <h1>Hi there!</h1>
+
+        </div>
+    </section>
+        
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
+
